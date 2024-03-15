840d715d2468a76c99431f57af4727dfc5eff477
sv1999109 <shivamverma4678@gmail.com>
2024-03-15 17:28:37 -0600

    Shivam verma second commit

--- gloship-main/storage/framework/views/293f00235606e5ff326de3add906103c02cf5834.php
+++
@@ -1,2 +0,0 @@
-<canvas id="<?php echo e($options['chart_name'] ?? 'myChart'); ?>" class="<?php echo e($options['style_class'] ?? ''); ?>"></canvas>
-<?php /**PATH /home/ansabook/glovi.ansabooks.com/vendor/laraveldaily/laravel-charts/src/views/html.blade.php ENDPATH**/ ?>
--- gloship-main/storage/framework/views/c6ae9f4bb8a092279a5e96a4625e002b8939b3cc.php
+++
@@ -1,9 +0,0 @@
-<?php if(Session::has('modal_open')): ?>
-    <script>
-        $(document).ready(function() {
-            $("#<?php echo e(Session::get('modal_open')); ?>").modal('show');
-            $(body).apend('<div class="modal-backdrop fade show"></div>');
-        });
-    </script>
-<?php endif; ?>
-<?php /**PATH /home/ansabook/glovi.ansabooks.com/resources/views/dashboard/modal.blade.php ENDPATH**/ ?>
