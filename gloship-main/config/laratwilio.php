00818694abe108e66d98794aa0416402047a1ccc
sv1999109 <93350481+sv1999109@users.noreply.github.com>
2024-04-18 18:16:23 -0600

    Delete gloship-main directory

--- gloship-main/config/laratwilio.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'account_sid' => env('TWILIO_ACCOUNT_SID'),
-    'auth_token' => env('TWILIO_AUTH_TOKEN'),
-    'sms_from' => env('TWILIO_SMS_FROM'),
-];
--- gloship-main/resources/views/dashboard/modal.blade.php
+++
@@ -1,8 +0,0 @@
-@if (Session::has('modal_open'))
-    <script>
-        $(document).ready(function() {
-            $("#{{ Session::get('modal_open') }}").modal('show');
-            $(body).apend('<div class="modal-backdrop fade show"></div>');
-        });
-    </script>
-@endif
